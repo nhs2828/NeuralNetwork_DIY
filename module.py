a4f2f6839cfec34701b6c04493c1e5997cf4ef2c
Hoang-Son NGUYEN <son@MBPdeHoangSon.lan>
2023-03-26 22:59:32 +0200

    fini partie 3

--- module.py
+++ module.py
@@ -73,6 +73,7 @@ class Linear(Module):
             jusqu’à son appel avec un pas de gradient_step
         """ 
         self._parameters -= gradient_step*self._gradient
+        self._parameters /= np.linalg.norm(self._parameters)
         self.zero_grad()
         
     def backward_update_gradient(self, input, delta):
@@ -84,6 +85,7 @@ class Linear(Module):
             input (array): z_h-1
             delta (_type_): _description_
         """
+        #print(f"LIN MAJ GRAD input {input.shape} delta {delta.shape}")
         #print("BW_grad_Lin",delta.T.shape, input.shape)
         gradient = delta.T@input
         #print(gradient.shape)
@@ -99,6 +101,7 @@ class Linear(Module):
             en fonction de l’entrée input et des deltas de la couche
             suivante delta
         """
+        #print(f"LIN input {input.shape} delta {delta.shape}")
         return np.dot(delta, self._parameters)
         #return np.dot(delta, self._parameters.T)
     
@@ -144,6 +147,7 @@ class TanH(Module): # (e(z)-e(-z)) / (e(z)+e(-z))
             en fonction de l’entrée input et des deltas de la couche
             suivante delta
         """
+        #print(f"TANH input {input.shape} delta {delta.shape}")
         dzda = (2/(np.exp(input)-np.exp(-input)))**2
         #print(input.shape)
         #print(dzda.shape)
@@ -190,7 +194,74 @@ class Sigmoide(Module): # 1 / (1+e(-z))
             en fonction de l’entrée input et des deltas de la couche
             suivante delta
         """
+        #print(f"SIG input {input.shape} delta {delta.shape}")
         dzda = np.exp(-input)/(1+np.exp(-input))**2
         #print(input.shape)
         #print(dzda.shape)
         return delta*dzda     
+
+def f_sig_seq(seq, X):
+  A = seq.forward(X)
+  return np.where(A>0.5,1,-1)
+
+
+class Sequentiel():
+  def __init__(self, *module):
+    self.net = module
+
+  def forward(self, input):
+    z = input
+    self.input = [input]
+    for m in self.net:
+      z = m.forward(z)
+      self.input.append(z)
+    return z
+  
+  def backward(self,loss, Y,gradient_step=0.001):
+    delta_ = loss.backward(Y,self.input[-1])
+    #print(len(self.input), len(self.net))
+    for i in range(len(self.net)-1,-1,-1): # BACKKKKWARDDDDDDDD
+      #print(type(self.net[i]).__name__)
+      if type(self.net[i]).__name__ != 'Linear': # si fonction d'activation
+        #print(f"SEQ ACTI {type(self.net[i]).__name__} input {self.input[i].shape} delta {delta_.shape}")
+        delta_ = self.net[i].backward_delta(self.input[i], delta_)
+      else: #Lineaire
+        #print("LIN")
+        #print(f"SEQ LIN {type(self.net[i]).__name__} input {self.input[i].shape} delta {delta_.shape}")
+        self.net[i].backward_update_gradient(self.input[i],delta_)
+        delta_ = self.net[i].backward_delta(self.input[i], delta_)
+        self.net[i].update_parameters(gradient_step=gradient_step)
+
+
+class Optim():
+  def __init__(self, net, loss, eps):
+    self.net = net
+    self.loss = loss
+    self.eps = eps
+
+  def step(self, batch_x, batch_y):
+    y_hat = self.net.forward(batch_x)
+    loss = self.loss.forward(batch_y, y_hat)
+    self.net.backward(self.loss, batch_y,gradient_step=self.eps)
+    return y_hat, loss
+
+
+def SGD(net, data, loss, eps=1e-4, batch_taille=40, nb_iter=5):
+  """Apprentissage du réseaux en utilisant mini-batch et descente de gradient
+  Arg:
+  net: le réseau de neurones
+  data: le jeu de données X, Y
+  loss: fonction cout
+  eps: pas de gradient
+  batch_taille: la taille de chaque batch
+  nb_inter: nombre d'itérations
+  """
+  opt = Optim(net, loss, eps)
+  n = len(data)
+  data_train = cp.deepcopy(data)
+  for i in range(nb_iter): # pour chaque epochs
+    np.random.shuffle(data_train)
+    tab_mini_batches = [data_train[k:k+batch_taille] for k in range(0, n, batch_taille)] #decouper en mini batches
+    for mini_batch in tab_mini_batches: # apprentissage sur chaque mini batch
+      batch_x, batch_y = np.array(mini_batch[:,0].tolist()), np.array(mini_batch[:,1].tolist()) # array de array -> array de liste
+      opt.step(batch_x, batch_y)
