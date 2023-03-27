ad70840042994485c24060ecf32c33917790816b
Hoang-Son NGUYEN <son@MacBook-Pro-de-Hoang-Son.local>
2023-03-28 01:44:33 +0200

    ajoute BCELoss + partie 5

--- module.py
+++ module.py
@@ -23,19 +23,32 @@ def logsoftmax(y):
 
 class SMCELoss(Loss):
     def forward(self, y, yhat):
-        """Coût cross-entropique
+        """Coût log soft max - cross-entropique
         y: indice de la classe à prédire, exp pour 1 exemple [0,0,0,1] -> classe 3
         y_hat: le vecteur de prédiction -> exp pour 1 exemple [0.1, 0.3, 0.4, 0.2]
         """
-        #-yhat_y + log(sum_i(exp(yhat_i)))
         return -np.sum(y*yhat, axis = 1) + np.log(np.sum(np.exp(yhat), axis = 1))
 
     def backward(self, y, yhat):
         """ Calculer le gradient du cout par rapport yhat
         """
-        exp_sum_exp = np.exp(yhat)/np.sum(np.exp(yhat), axis = 1)[:, np.newaxis] # 1/sum(exp(yhat_i))
+        exp_sum_exp = np.exp(yhat)/np.sum(np.exp(yhat), axis = 1)[:, np.newaxis] # exp(yhat_k)/sum(exp(yhat_i))
         tmp_2 = np.where(y==1, -1, 0)
-        return exp_sum_exp + tmp_2 # ou y = 1 -> -1
+        return exp_sum_exp + tmp_2 # où y = 1 -> -1
+
+class BCELoss(Loss):
+    def forward(self, y, yhat):
+        """Coût cross-entropique binaire
+        """
+        return -(y*np.log(yhat) + (1-y)*np.log(1-yhat))
+
+    def backward(self, y, yhat):
+        """ Calculer le gradient du cout par rapport yhat
+        """
+        yhat = np.where(yhat == 0, 1e-9, yhat)
+        yhat = np.where(yhat == 1, 0.999999999, yhat) # smoothing
+        return -(y/yhat + (y-1)/(1-yhat))
+
 
 class Module(object):
     def __init__(self):
